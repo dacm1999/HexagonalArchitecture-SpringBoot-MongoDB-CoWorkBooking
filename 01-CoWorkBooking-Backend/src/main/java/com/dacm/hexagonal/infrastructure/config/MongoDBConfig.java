a2266820e794e42314cab2f353604d4e8e4a9974
dacm1999 <danielcontreras12.dc@gmail.com>
2024-05-17 20:10:33 +0200

    Environment Variables and Metadata, as well as Adding Classes for MongoDBConfig

---
+++ 01-CoWorkBooking-Backend/src/main/java/com/dacm/hexagonal/infrastructure/config/MongoDBConfig.java
@@ -0,0 +1,45 @@
+package com.dacm.hexagonal.infrastructure.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.dacm.hexagonal.infrastructure.adapters.output.persistence.repository")
+public class MongoDBConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${v.mongodb.host}")
+    private String host;
+
+    @Value("${v.mongodb.port}")
+    private int port;
+
+    @Value("${v.mongodb.database}")
+    private String databaseName;
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString("mongodb://" + host + ":" + port + "/" + databaseName);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
+    }
+}
